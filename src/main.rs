652c403d12a736755f0b5a7b89e58ad0fb762478
mihaimindrescu <mindrescumihai99@gmail.com>
2025-05-08 22:40:17 +0300

    Add VGA text buffer abstraction and println macro

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
 #![no_std]
 #![no_main]
-use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello, world!";
+mod vga_buffer;
+
+use core::panic::PanicInfo;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello, World{}", "!");
+    
     loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
